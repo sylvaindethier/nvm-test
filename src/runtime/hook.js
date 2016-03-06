486b3b06b24f41e007209ca72a3d187b7eefed91
sylvaindethier <dethiersylvain@gmail.com>
2016-03-06 15:50:50 +0100

    rewrite hook

--- src/runtime/hook.js
+++ src/runtime/hook.js
@@ -1,33 +1,18 @@
 /**
- * Set a function name
- * @param {String} name - A name for the function
- * @param {Function} fn - A function to set the name
- * @return {Function} The function renamed
- */
-function setFunctionName (name, fn) {
-  // rename async function from promise function name
-  const fnName = Object.getOwnPropertyDescriptor(fn, 'name')
-  fnName.value = name.toString()
-  Object.defineProperty(fn, 'name', fnName)
-
-  return fn
-}
-
-/**
- * Make a promise hookable ('pre', 'post', 'error')
- * @param {Promise} promise - A promise
+ * Make a function hookable ('pre', 'post', 'error')
+ * @param {Function} fn - A function to hook
  * @return {Function} The promise to be called with hooks ('$pre', '$post', '$error')
  */
-function hook (promise) {
-  const name = promise.name
-  return setFunctionName(`${name}Hook`, function (...args) {
-    return setFunctionName(`${name}Async`, async function ({ $pre, $post, $error } = {}) {
+export default function hook (fn) {
+  if (typeof fn !== 'function') throw new TypeError('Expected a function')
+  return function (...args) {
+    return async function ({ $pre, $post, $error } = {}) {
       try {
         // $pre hook
         if (typeof $pre === 'function') $pre(...args)
 
-        // await for promise
-        const ret = await promise(...args)
+        // await for function
+        const ret = await fn(...args)
 
         // $post hook
         if (typeof $post === 'function') $post(ret, ...args)
@@ -40,8 +25,6 @@ function hook (promise) {
         // re throw error
         throw e
       }
-    })
-  })
+    }
+  }
 }
-
-export { hook as default, setFunctionName }
--- test/runtime/hook.spec.js
+++ test/runtime/hook.spec.js
@@ -1,20 +1,70 @@
 import expect from 'expect'
-import hook, { setFunctionName } from '../../src/runtime/hook'
+import hook from '../../src/runtime/hook'
 
-describe('setFunctionName', function () {
-  it('should be a function', function () {
-    expect(setFunctionName).toBeA('function')
-  })
-
-  it('should return a renamed function', function () {
-    function foo () {} // eslint-disable-line require-jsdoc
-    const name = 'bar'
-    expect(setFunctionName(name, foo).name).toEqual(name)
-  })
-})
+const fn = (...args) => { return args }
 
 describe('hook', function () {
   it('should be a function', function () {
     expect(hook).toBeA('function')
+  })
+
+  it('should throw a TypeError if argument is not a function', function () {
+    expect(function () { hook() }).toThrow(TypeError)
+  })
+
+  it('should return a function at 1st level', function () {
+    expect(hook(fn)).toBeA('function')
+  })
+
+  it('should return a function at 2nd level', function () {
+    expect(hook(fn)()).toBeA('function')
+  })
+
+  it('should return a Promise at 3rd level', function () {
+    expect(hook(fn)()()).toBeA(Promise)
+  })
+
+  it('should execute a $pre hook', function () {
+    const hooks = { $pre: () => {} }
+    const spy = expect.spyOn(hooks, '$pre')
+
+    return hook(fn)('foo', 'bar')(hooks).then(() => {
+      expect(spy).toHaveBeenCalledWith('foo', 'bar')
+      expect.restoreSpies()
+    })
+  })
+
+  it('should execute a $post hook', function () {
+    const hooks = { $post: () => {} }
+    const spy = expect.spyOn(hooks, '$post')
+    const add = (a, b) => (a + b)
+
+    return hook(add)('foo', 'bar')(hooks).then(() => {
+      expect(spy).toHaveBeenCalledWith('foobar', 'foo', 'bar')
+      expect.restoreSpies()
+    })
+  })
+
+  it('should execute a $error hook on error', function () {
+    const hooks = { $error: () => {} }
+    const spy = expect.spyOn(hooks, '$error')
+    const e = new Error()
+    const err = () => { throw e }
+
+    return hook(err)('foo', 'bar')(hooks).catch(() => {
+      expect(spy).toHaveBeenCalledWith(e, 'foo', 'bar')
+      expect.restoreSpies()
+    })
+  })
+
+  it('should reject on Error', function () {
+    const err = () => { throw new Error() }
+    return hook(err)()()
+    .then(() => {
+      throw new Error('Error was resolved, it should NOT')
+    })
+    .catch((e) => {
+      expect(e).toBeA(Error)
+    })
   })
 })
