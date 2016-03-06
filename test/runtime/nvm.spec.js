2ebdd4559e2049e3d8e5ed9aa975d4901799f9fd
sylvaindethier <dethiersylvain@gmail.com>
2016-03-06 06:21:19 +0100

    fix test ;-)

--- test/runtime/nvm.spec.js
+++ test/runtime/nvm.spec.js
@@ -1,22 +1,43 @@
 import expect from 'expect'
-import { execSync, ChildProcess } from 'child_process'
+import { renameSync } from 'fs'
 import nvmHook, { nvm, exists, shell } from '../../src/runtime/nvm'
+
+import { spawn } from 'child_process'
+import { statSync } from 'fs'
+
+// ChildProcess class is not exposed under Node 0.12, use EventEmitter instead
+let ChildProcess = require('child_process').ChildProcess
+if (ChildProcess === undefined) ChildProcess = require('events').EventEmitter
+
+describe('Node used import', function () {
+  it('child_process.spawn should exists', function () {
+    expect(spawn).toExist()
+  })
+
+  it('fs.statSync should exists', function () {
+    expect(statSync).toExist()
+  })
+
+  it('fs.renameSync should exists', function () {
+    expect(renameSync).toExist()
+  })
+})
 
 describe('exists', function () {
   it('should be a function', function () {
     expect(exists).toBeA('function')
   })
 
-  it('should return false if NVM does not exists', function () {
-    // move NVM
+  it('should return false if nvm does not exists', function () {
+    // move NVM_DIR
     const tmp = process.env.NVM_DIR + '.bck'
-    execSync(`mv ${process.env.NVM_DIR} ${tmp}`)
+    renameSync(process.env.NVM_DIR, tmp)
     expect(exists()).toBe(false)
-    // restablish NVM
-    execSync(`mv ${tmp} ${process.env.NVM_DIR}`)
+    // restablish NVM_DIR
+    renameSync(tmp, process.env.NVM_DIR)
   })
 
-  it('should return true if NVM exists', function () {
+  it('should return true if nvm exists', function () {
     expect(exists()).toBe(true)
   })
 })
@@ -27,7 +48,12 @@ describe('shell', function () {
   })
 
   it('should return a ChildProcess', function () {
-    expect(shell('echo "shell test" > /dev/null')).toBeA(ChildProcess)
+    expect(shell()).toBeA(ChildProcess)
+  })
+
+  it('should execute with success', function (done) {
+    // close with done function
+    shell().on('close', done)
   })
 })
 
@@ -37,7 +63,9 @@ describe('nvm', function () {
   })
 
   it('should return a Promise', function () {
-    expect(nvm('echo "nvm test" > /dev/null')).toBeA(Promise)
+    const sh = nvm('echo "nvm test" > /dev/null')
+    expect(sh).toBeA(Promise)
+    return sh
   })
 
   it('should reject the Promise on error', function () {
