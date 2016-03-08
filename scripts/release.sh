519ffbc48a280bb2135851b01120cd4c1788a618
sylvaindethier <dethiersylvain@gmail.com>
2016-03-08 21:07:03 +0100

    fix(release): conform to common release process

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,9 +6,6 @@ script='scripts/release.sh'
   echo >&2 "Please run $script from the repo root" &&
   exit 1
 
-## run the build
-npm run build
-
 ## get the current version from package
 current_version=$(node --print "require('./package').version")
 
@@ -18,19 +15,12 @@ echo "Update type: <version> | (pre)major | (pre)minor | (pre)patch | prerelease
 printf "Which update type is ? "
 read update_type
 
-## update package version (disable git-tag-version, will be done after)
-version=$(npm --no-git-tag-version version $update_type)
+## update version
+version=$(npm version $update_type)
 echo "New version is $version"
 
-## add and commit unstaged changes
-git add --all
-git commit --allow-empty -am "release $update_type (from $current_version)"
-
-## create tag version
-git tag -a "$version" -m "version $version"
-
-## push to origin with tag
-git push --follow-tag origin master
+## push with tags
+git push --follow-tags
 
 ## publish to npm
 npm publish
