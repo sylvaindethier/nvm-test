5d0547b7585c0a512bbcb47b37ecdee37852beaf
sylvaindethier <dethiersylvain@gmail.com>
2016-03-15 21:06:29 +0100

    remove expose test

--- test/runtime/00-index.spec.js
+++ test/runtime/00-index.spec.js
@@ -1,28 +1,5 @@
 import expect from 'expect'
-import * as runtime from '../../src/runtime'
 import { renameSync, statSync } from 'fs'
-
-describe('runtime', function () {
-  it('should expose a "config" object', function () {
-    expect(runtime.config).toBeA('object')
-  })
-
-  it('should expose a "nvmInstall" function', function () {
-    expect(runtime.nvmInstall).toBeA('function')
-  })
-
-  it('should expose a "nvmTest" function', function () {
-    expect(runtime.nvmTest).toBeA('function')
-  })
-
-  it('should expose a "nvmTestVersion" function', function () {
-    expect(runtime.nvmTestVersion).toBeA('function')
-  })
-
-  it('should expose a "nvmTestVersions" function', function () {
-    expect(runtime.nvmTestVersions).toBeA('function')
-  })
-})
 
 describe('runtime', function () {
   it('sould fail to require if nvm does not exists', function () {
